e8a7c070f8392effa0b13903ed64b2cbc887de01
alijavaboy <ali77erfagh99@gmail.com>
2022-03-11 20:46:29 +0330

    refactore test

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -5,71 +5,38 @@ import org.junit.jupiter.api.Test;
 class FizzBuzzTest {
     @Test
     public void play() {
-        play_give_3();
-        play_give_33();
-        play_give_12();
-        play_give_5();
-        play_give_10();
-        play_give_25();
-        play_give_15();
-        play_give_30();
+        bakhpazerBar_3();
+        bakhpazerBar_5();
+        bakhshpazirBar_3_va_5();
     }
 
     @Test
-    public void play_give_3() {
+    private void bakhpazerBar_3() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.play(3);
-        Assertions.assertEquals("Fizz", result);
+        String result1 = fizzBuzz.play(3);
+        Assertions.assertEquals("Fizz", result1);
+        String result2 = fizzBuzz.play(33);
+        Assertions.assertEquals("Fizz", result2);
+        String result3 = fizzBuzz.play(12);
+        Assertions.assertEquals("Fizz", result3);
     }
-
     @Test
-    public void play_give_33() {
+    private void bakhpazerBar_5() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.play(33);
-        Assertions.assertEquals("Fizz", result);
+        String result1 = fizzBuzz.play(3);
+        Assertions.assertEquals("Fizz", result1);
+        String result2 = fizzBuzz.play(33);
+        Assertions.assertEquals("Fizz", result2);
+        String result3 = fizzBuzz.play(12);
+        Assertions.assertEquals("Fizz", result3);
     }
-
-    @Test
-    public void play_give_12() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.play(12);
-        Assertions.assertEquals("Fizz", result);
-    }
-
-    @Test
-    public void play_give_5() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.play(5);
-        Assertions.assertEquals("Buzz", result);
-    }
-
-    @Test
-    public void play_give_10() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.play(10);
-        Assertions.assertEquals("Buzz", result);
-    }
-
-    @Test
-    public void play_give_25() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.play(25);
-        Assertions.assertEquals("Buzz", result);
-    }
-
-    @Test
-    public void play_give_15() {
+    private void bakhshpazirBar_3_va_5(){
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.play(15);
         Assertions.assertEquals("FizzBuzz", result);
+        String result2 = fizzBuzz.play(30);
+        Assertions.assertEquals("FizzBuzz", result2);
+        String result3 = fizzBuzz.play(45);
+        Assertions.assertEquals("FizzBuzz", result3);
     }
-
-    @Test
-    public void play_give_30() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.play(30);
-        Assertions.assertEquals("FizzBuzz", result);
-    }
-
-
 }
