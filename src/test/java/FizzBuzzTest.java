416a3c2f709e3f954691b5f53f7efad390010bcb
alijavaboy <ali77erfagh99@gmail.com>
2022-03-11 20:57:23 +0330

    add static assert

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,10 +1,12 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class FizzBuzzTest {
     FizzBuzz fizzBuzz;
+
     @BeforeEach
     private void beforeEach() {
         fizzBuzz = new FizzBuzz();
@@ -20,29 +22,29 @@ class FizzBuzzTest {
     @Test
     private void bakhpazerBar_3() {
         String result1 = fizzBuzz.play(3);
-        Assertions.assertEquals("Fizz", result1);
+        assertEquals("Fizz", result1);
         String result2 = fizzBuzz.play(33);
-        Assertions.assertEquals("Fizz", result2);
+        assertEquals("Fizz", result2);
         String result3 = fizzBuzz.play(12);
-        Assertions.assertEquals("Fizz", result3);
+        assertEquals("Fizz", result3);
     }
 
     @Test
     private void bakhpazerBar_5() {
         String result1 = fizzBuzz.play(5);
-        Assertions.assertEquals("Buzz", result1);
+        assertEquals("Buzz", result1);
         String result2 = fizzBuzz.play(10);
-        Assertions.assertEquals("Buzz", result2);
+        assertEquals("Buzz", result2);
         String result3 = fizzBuzz.play(25);
-        Assertions.assertEquals("Buzz", result3);
+        assertEquals("Buzz", result3);
     }
 
     private void bakhshpazirBar_3_va_5() {
         String result = fizzBuzz.play(15);
-        Assertions.assertEquals("FizzBuzz", result);
+        assertEquals("FizzBuzz", result);
         String result2 = fizzBuzz.play(30);
-        Assertions.assertEquals("FizzBuzz", result2);
+        assertEquals("FizzBuzz", result2);
         String result3 = fizzBuzz.play(45);
-        Assertions.assertEquals("FizzBuzz", result3);
+        assertEquals("FizzBuzz", result3);
     }
 }
